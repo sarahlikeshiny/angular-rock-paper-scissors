af7d6e491421bf22776d9939f92c75bad4e20796
sarahlikeshiny <sarah.g.thomas@gmail.com>
2017-06-03 17:40:43 +0100

    game logic works

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ angular
   .module('rpsApp', [])
   .controller('MainCtrl', MainCtrl);
 
+// MainCtrl.$inject = [];
 function MainCtrl() {
   const vm = this;
   vm.choices = [
@@ -11,13 +12,46 @@ function MainCtrl() {
     {option: 'scissors'},
     {option: 'paper'}
   ];
+  vm.player1Choice = '';
+  vm.player2Choice = '';
+  vm.winner = '';
 
   vm.getChoice = getChoice;
 
-  function getChoice(choice) {
-    console.log('choice',choice.option);
-    vm.choice = choice.option;
+  function getChoice(choice) {//gets the player's choice and attach to scope
+    const player1choice = choice.option;
+    // console.log('choice', player1choice);
+
+    const winConditions = {//set win conditions
+      rock: ['scissors'],
+      paper: ['rock'],
+      scissors: ['paper']
+    };
+    const conditions =Object.keys(winConditions);
+    //randomly generate computer's choice
+    const player2Choice = conditions[Math.floor(Math.random() * conditions.length)];
+    // console.log('player2', player2Choice);
+
+    function findWinner(player1Choice, player2Choice) {//find winner
+      if (winConditions[player1Choice] .includes(player2Choice)) return 'You Win';
+      if (winConditions[player2Choice] .includes(player1Choice)) return 'You Lose';
+      return 'Tie';
+    }
+
+    const winner = findWinner(player1choice, player2Choice);
+    // console.log('winner', winner);
+
+    //attach choices and winner to scope
+    vm.player1Choice = player1choice;
+    vm.player2Choice = player2Choice;
+    vm.winner = winner;
+  }
+  function reset() {
+    vm.player1Choice = '';
+    vm.player2Choice = '';
+    vm.winner = '';
   }
 
+  vm.reset = reset;
 
 }
